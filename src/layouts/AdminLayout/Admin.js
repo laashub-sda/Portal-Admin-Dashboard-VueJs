18289384aeb5aa52ca2d4abcb8e66159718a56a5
Gentrit Abazi <gentritabazi01@gmail.com>
2021-01-17 17:59:29 +0100

    Fix bootstrap not working

--- src/layouts/AdminLayout/Admin.js
+++ src/layouts/AdminLayout/Admin.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/js/bootstrap.min.js'
+import '@popperjs/core'
+// import 'bootstrap/dist/js/bootstrap.min.js' // Not Working
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/portal.css'
 import sidePanelComponent from '@/components/SidePanelComponent'
@@ -9,5 +10,14 @@ export default {
     components: {
         sidePanelComponent,
         headerComponent
+    },
+    created() {
+        let popper = document.createElement('script');
+        let bootstrap = document.createElement('script');
+        bootstrap.setAttribute('src', 'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js');
+        popper.setAttribute('src', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+
+        document.head.appendChild(popper);
+        document.head.appendChild(bootstrap);
     }
 }
